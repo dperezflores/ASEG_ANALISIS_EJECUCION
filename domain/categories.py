f73d3814eafcfcf3bc85ea39aacbda1f4f43b028
dperezflores <daniel.apf99@gmail.com>
2026-09-07 15:06:34 -0600

    Agrega categoría Solicitudes de Pago

--- domain/categories.py
+++ domain/categories.py
@@ -6,6 +6,7 @@ from enum import StrEnum
 class CategoriaDocumento(StrEnum):
     ESTIMACIONES = "Estimaciones"
     FACTURAS = "Facturas"
+    SOLICITUDES_PAGO = "Solicitudes de Pago"
     COMPROBANTES = "Comprobantes de Pago"
     POLIZAS = "Pólizas"
 
